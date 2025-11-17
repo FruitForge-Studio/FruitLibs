f6d3581debab348da46d150f51371a9ed121158c
Ricardo Soto <vyrbyte@gmail.com>
2025-11-16 23:02:12 -0600

    feat: add project structure and plugin files

---
+++ src/main/java/com/fruitforge/fruitLibs/FruitLibs.java
@@ -0,0 +1,18 @@
+package com.fruitforge.fruitLibs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FruitLibs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
+
