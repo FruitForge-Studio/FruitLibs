7464038941d0b2bdc6945dd6fde7c8893a5b65a9
Ricardo Soto <vyrbyte@gmail.com>
2025-11-17 16:16:22 -0600

    feat(command): implement SubcommandNode with reflection execution

--- src/main/java/com/fruitforge/fruitLibs/core/command/SubcommandNode.java
+++ src/main/java/com/fruitforge/fruitLibs/core/command/SubcommandNode.java
@@ -1,4 +1,45 @@
 package com.fruitforge.fruitLibs.core.command;
 
+import com.fruitforge.fruitLibs.api.command.Subcommand;
+
+import java.lang.reflect.Method;
+
 public class SubcommandNode {
+
+    private final Subcommand subcommand;
+    private final Method method;
+    private final Object instance;
+    private final String[] path;
+
+    public SubcommandNode(Subcommand subcommand, Method method, Object instance) {
+        this.subcommand = subcommand;
+        this.method = method;
+        this.instance = instance;
+        this.path = subcommand.value().split(" ");
+        this.method.setAccessible(true);
+    }
+
+    public void execute(Object... args) throws Exception {
+        method.invoke(instance, args);
+    }
+
+    public Subcommand getSubcommand() {
+        return subcommand;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public Object getInstance() {
+        return instance;
+    }
+
+    public String[] getPath() {
+        return path;
+    }
+
+    public int getPathLength() {
+        return path.length;
+    }
 }
