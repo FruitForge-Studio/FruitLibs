1680a2dbab16c7d20249545bff24f8612897f730
Ricardo Soto <vyrbyte@gmail.com>
2025-11-17 13:23:36 -0600

    feat: implement core managers initialization and API getters in FruitLibs plugin

--- src/main/java/com/fruitforge/fruitLibs/FruitLibs.java
+++ src/main/java/com/fruitforge/fruitLibs/FruitLibs.java
@@ -1,29 +1,64 @@
+// src/main/java/com/fruitforge/fruitLibs/FruitLibs.java
 package com.fruitforge.fruitLibs;
 
+import com.fruitforge.fruitLibs.api.config.IConfigManager;
+import com.fruitforge.fruitLibs.api.messages.IMessageManager;
+import com.fruitforge.fruitLibs.core.config.ConfigManager;
+import com.fruitforge.fruitLibs.core.logging.LogManager;
+import com.fruitforge.fruitLibs.core.messages.MessageManager;
 import revxrsal.zapper.ZapperJavaPlugin;
 
 public final class FruitLibs extends ZapperJavaPlugin {
 
     private static FruitLibs instance;
 
+    private LogManager logManager;
+    private ConfigManager configManager;
+    private MessageManager messageManager;
+
     @Override
     public void onEnable() {
         instance = this;
 
         long startTime = System.currentTimeMillis();
-        getLogger().info("Initializing FruitLibs v" + getDescription().getVersion());
+
+        initializeManagers();
 
         long endTime = System.currentTimeMillis();
-        getLogger().info("FruitLibs loaded successfully in " + (endTime - startTime) + "ms");
+        logManager.success("FruitLibs v" + getDescription().getVersion() +
+                " loaded in " + (endTime - startTime) + "ms");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("FruitLibs disabled.");
+        logManager.info("FruitLibs disabled.");
         instance = null;
+    }
+
+    private void initializeManagers() {
+        logManager = new LogManager(this);
+        logManager.info("Initializing FruitLibs...");
+
+        configManager = new ConfigManager(this, logManager);
+        logManager.debug("ConfigManager initialized");
+
+        messageManager = new MessageManager(configManager, logManager);
+        logManager.debug("MessageManager initialized");
     }
 
     public static FruitLibs getInstance() {
         return instance;
+    }
+
+    public IConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public IMessageManager getMessageManager() {
+        return messageManager;
+    }
+
+    public LogManager getLogManager() {
+        return logManager;
     }
 }
