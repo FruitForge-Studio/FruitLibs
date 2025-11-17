f41b39023ccd722d04ea450450e2b368575f4d51
Ricardo Soto <vyrbyte@gmail.com>
2025-11-17 14:45:20 -0600

    feat(api): define DatabaseType enum with driver and URL templates

--- src/main/java/com/fruitforge/fruitLibs/api/database/DatabaseType.java
+++ src/main/java/com/fruitforge/fruitLibs/api/database/DatabaseType.java
@@ -1,4 +1,24 @@
 package com.fruitforge.fruitLibs.api.database;
 
-public class DatabaseType {
+public enum DatabaseType {
+    SQLITE("org.sqlite.JDBC", "jdbc:sqlite:{file}"),
+    MYSQL("com.mysql.cj.jdbc.Driver", "jdbc:mysql://{host}:{port}/{database}?autoReconnect=true&useSSL=false&serverTimezone=UTC"),
+    MARIADB("org.mariadb.jdbc.Driver", "jdbc:mariadb://{host}:{port}/{database}?autoReconnect=true"),
+    POSTGRESQL("org.postgresql.Driver", "jdbc:postgresql://{host}:{port}/{database}");
+
+    private final String driverClass;
+    private final String urlTemplate;
+
+    DatabaseType(String driverClass, String urlTemplate) {
+        this.driverClass = driverClass;
+        this.urlTemplate = urlTemplate;
+    }
+
+    public String getDriverClass() {
+        return driverClass;
+    }
+
+    public String getUrlTemplate() {
+        return urlTemplate;
+    }
 }
